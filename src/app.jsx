1d78bc08e23958849b2423687bbd961d3dc58110
Lance Shi <Lanceyshi@gmail.com>
2017-12-14 22:09:17 +1000

    add state into component

--- src/app.jsx
+++ src/app.jsx
@@ -79,13 +79,39 @@ class IssueAdd extends React.Component {
 }
 
 class IssueList extends React.Component {
+    constructor() {
+        super();
+        this.state = {issues: issues};
+
+        for(var i = 0; i < 5; i++) {
+            setTimeout(this.createTestIssue.bind(this), 2000 * i);
+        }
+        
+    }
+
+    createIssue(newIssue) {
+        const newIssues = this.state.issues.slice();
+        newIssue.id = this.state.issues.length + 1;
+        newIssues.push(newIssue);
+        this.setState({issues: newIssues});
+    }
+
+    createTestIssue() {
+        this.createIssue({
+            status:'New',
+            owner: 'Pieta',
+            created: new Date(),
+            title: 'Completion date should be optional'
+        });
+    }
+
     render() {
         return (
             <div>
                 <h1>Issue Tracker</h1>
                 <IssueFilter/>
                 <hr/>
-                <IssueTable issues={issues}/>
+                <IssueTable issues={this.state.issues}/>
                 <hr/>
                 <IssueAdd/>
             </div>
